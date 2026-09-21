47a3f69bc53322123fb2b4b51ad39a62b984d080
zertexa <zectxrz@gmail.com>
2026-09-21 22:59:59 +0700

    cell02/ex01

---
+++ discovery_piscine/cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+number = int(input())
+if number < 0:
+    print("This number is negative.")
+elif number > 0:
+    print("This number is positive")
+elif number == 0:
+    print("This number is both positive and negative.")
