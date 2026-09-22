2f907f9d2e9bedb296c6ae6381afa1855fd6a818
zxrtexa <zectxrz@gmail.com>
2026-09-22 19:08:26 +0700

    cell04/ex02

---
+++ discovery_piscine/cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+first = int(input("Give me the first number: "))
+second = int(input("Give me the second number: "))
+print("Thank you!")
+print(f"{first} + {second} = {first+second}")
+print(f"{first} - {second} = {first-second}")
+print(f"{first} / {second} = {int(first/second)}")
+print(f"{first} * {second} = {first*second}")
