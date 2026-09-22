ec0c6561ae1fe75a6eeb2de10cb6b62ee32bb224
zxrtexa <zectxrz@gmail.com>
2026-09-22 18:58:20 +0700

    cell04/ex01

---
+++ discovery_piscine/cell04/ex01/age.py
@@ -0,0 +1,6 @@
+age = int(input("Please tell me your age: "))
+print(f"You are currently {age} years old.")
+for i in range(0,3):
+    year = (i+1)*10
+    print(f"In {year} years, you'll be {age+year} years old.")
+    i+=1
